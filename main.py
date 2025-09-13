da2d5708cac7b858fa75d19a8011eb6908ea0510
Timothy Tam <imtimtam@gmail.com>
2025-09-12 17:21:00 -0700

    Update API to more efficient dict-based mapping to combine endpoints

--- main.py
+++ main.py
@@ -12,50 +12,80 @@ twosides_df = pd.read_csv(twosides_path)
 # Initialize API
 app = FastAPI()
 
+# RESPONSE MODELS
 class TargetResponse(BaseModel):
     interaction: bool
-    target: Optional[str] = Field(default=None, description="Shared target of drugs")
+    targets: Optional[list[str]] = Field(default=None, description="Shared receptor target(s) of drugs")
 
 class DDIResponse(BaseModel):
     interaction: bool
     conditions_and_prr: Optional[dict[str, float]] = Field(default=None, description="Condition : PRR dictionary")
 
+class CombinedResponse(BaseModel):
+    interaction: bool
+    targets: Optional[list[str]] = Field(default=None, description="Shared receptor target(s) of drugs")
+    conditions_and_prr: Optional[dict[str, float]] = Field(default=None, description="Condition : PRR dictionary")
+
+# PREPROCESSING
 # Target Preprocessing
-shared_targets = target_df.groupby(['Target', 'Target ID'])['Ligand'].apply(lambda x: set(l.lower() for l in x)).reset_index()
+target_df['Ligand'] = target_df['Ligand'].str.strip().str.lower()
+
+targets_dict = {}
+for target, group in target_df.groupby("Target"):
+    ligands = set(group['Ligand'])
+    for d1 in ligands:
+        for d2 in ligands:
+            if d1 < d2:
+                pair = frozenset([d1, d2])
+                if pair not in targets_dict:
+                    targets_dict[pair] = []
+                targets_dict[pair].append(target)
 
 # TWOSIDES Preprocessing
-twosides_df["drug_1_concept_name"] = twosides_df["drug_1_concept_name"].str.lower()
-twosides_df["drug_2_concept_name"] = twosides_df["drug_2_concept_name"].str.lower()
+twosides_df["drug_1_concept_name"] = twosides_df["drug_1_concept_name"].str.strip().str.lower()
+twosides_df["drug_2_concept_name"] = twosides_df["drug_2_concept_name"].str.strip().str.lower()
 
 twosides_df["pair"] = twosides_df.apply(lambda x: frozenset([x["drug_1_concept_name"], x["drug_2_concept_name"]]), axis=1)
-twosides_dict = (twosides_df.groupby("pair").apply(lambda x: dict(zip(x["condition_concept_name"], x["PRR"]))).to_dict())
+twosides_dict = (twosides_df.groupby("pair")[["condition_concept_name", "PRR"]].apply(lambda x: dict(zip(x["condition_concept_name"], x["PRR"]))).to_dict())
 
-# Return TargetResponse if drugs share a target
+# ENDPOINTS
+# Return shared receptor target(s) if drugs bind to the same target
 @app.get('/targets/', response_model=TargetResponse)
 def check_target(drug1: str = Query(..., min_length=2, max_length=50, description="First drug name"),
                  drug2: str = Query(..., min_length=2, max_length=50, description="Second drug name")):
-    drug1, drug2 = drug1.lower(), drug2.lower()
+    pair = frozenset([drug1.strip().lower(), drug2.strip().lower()])
+    target = targets_dict.get(pair)
 
-    # iterrows() for now due to smaller dataset compared to TWOSIDES
-    for _, row in shared_targets.iterrows():
-        ligands = row["Ligand"]
-        if drug1 in ligands and drug2 in ligands:
-            return TargetResponse(
-                interaction=True,
-                target=row["Target"],
-            )
+    if target:
+        return TargetResponse(interaction=True, targets=target)
 
     return TargetResponse(interaction=False)
 
-# Return Conditions and PRR if drugs have a DDI
+# Return conditions and PRR if drugs have a DDI
 @app.get('/interactions/', response_model=DDIResponse)
 def check_ddi(drug1: str = Query(..., min_length=2, max_length=50, description="First drug name"),
               drug2: str = Query(..., min_length=2, max_length=50, description="Second drug name")):
-    pair = frozenset([drug1.lower(), drug2.lower()])
-
+    pair = frozenset([drug1.strip().lower(), drug2.strip().lower()])
     conditions_and_prr = twosides_dict.get(pair)
+
     if conditions_and_prr:
         sorted_conditions_and_prr = dict(sorted(conditions_and_prr.items(), key=lambda items: items[1], reverse=True))
         return DDIResponse(interaction=True, conditions_and_prr=sorted_conditions_and_prr)
 
     return DDIResponse(interaction=False)
+
+# Return receptor targets(s), conditions and PRR if they exist and drugs have a DDI
+@app.get('/full_interactions/', response_model=CombinedResponse)
+def check_combined(drug1: str = Query(..., min_length=2, max_length=50, description="First drug name"),
+                   drug2: str = Query(..., min_length=2, max_length=50, description="Second drug name")):
+    pair = frozenset([drug1.strip().lower(), drug2.strip().lower()])
+    target = targets_dict.get(pair)
+    conditions_and_prr = twosides_dict.get(pair)
+
+    sorted_conditions_and_prr = None
+    if conditions_and_prr:
+        sorted_conditions_and_prr = dict(sorted(conditions_and_prr.items(), key=lambda items: items[1], reverse=True))
+    if target or sorted_conditions_and_prr:
+        return CombinedResponse(interaction=True, targets=target, conditions_and_prr=sorted_conditions_and_prr)
+    
+    return CombinedResponse(interaction=False)
